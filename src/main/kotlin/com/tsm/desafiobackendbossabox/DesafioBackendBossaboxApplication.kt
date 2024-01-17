1cdc03d74c0b746ada6c482864e0f85fbd07f2eb
Thierry <thierrydev48@gmail.com>
2024-01-17 17:34:53 -0300

    first commit

---
+++ src/main/kotlin/com/tsm/desafiobackendbossabox/DesafioBackendBossaboxApplication.kt
@@ -0,0 +1,11 @@
+package com.tsm.desafiobackendbossabox
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DesafioBackendBossaboxApplication
+
+fun main(args: Array<String>) {
+	runApplication<DesafioBackendBossaboxApplication>(*args)
+}
